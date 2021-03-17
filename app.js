33c5edd169c9194b74dd57d9e190f32e31eb8d59
David Wu <david204code@gmail.com>
2021-03-17 17:26:53 +0000

    Add the EventListener of submit to form and include e.preventDefault()

--- app.js
+++ app.js
@@ -15,3 +15,8 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
 const form = document.querySelector(".lorem-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // console.log("Submit form");
+});
